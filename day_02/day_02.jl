c43fdcd7ddf8a1bbfbf5de34c1f3f2ef577a04cc
Fernando <fepegar@gmail.com>
2018-01-02 02:04:24 +0000

    Add day 2

---
+++ day_02/day_02.jl
@@ -0,0 +1,25 @@
+input_path = joinpath(dirname(Base.source_path()), "input.txt")
+f = open(input_path)
+lines = readlines(f)
+close(f)
+
+paper = 0
+ribbon = 0
+for line in lines
+    dimensions = split(line, "x")
+    dimensions = map(x->parse(Int32, x), dimensions)
+    sort!(dimensions)  # sort in-place
+    x, y, z = dimensions
+    a = 2 * x * y
+    b = 2 * x * z
+    c = 2 * y * z
+    surface = a + b + c
+    smallest = Int32(minimum([a, b, c]) / 2)
+    paper += surface + smallest
+
+    volume = reduce(*, dimensions)
+    perimeter = 2 * (x + y)
+    ribbon += volume + perimeter
+end
+println("Paper: ", paper)
+println("Ribbon: ", ribbon)
