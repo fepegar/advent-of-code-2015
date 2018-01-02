30aed9c1890d42dc4825c97587f6148ee4dbe3e8
Fernando <fepegar@gmail.com>
2018-01-02 02:36:24 +0000

    Wrap everything into functions

--- day_02/day_02.jl
+++ day_02/day_02.jl
@@ -3,28 +3,36 @@ function get_volume(dimensions)
     reduce(*, dimensions)
 end
 
-input_path = joinpath(dirname(Base.source_path()), "input.txt")
-f = open(input_path)
-lines = readlines(f)
-close(f)
-
-paper = 0
-ribbon = 0
-for line in lines
-    dimensions = split(line, "x")
-    dimensions = map(x->parse(Int32, x), dimensions)
-    sort!(dimensions)  # sort in-place
-    x, y, z = dimensions
-    a = 2 * x * y
-    b = 2 * x * z
-    c = 2 * y * z
-    surface = a + b + c
-    smallest = Int32(minimum([a, b, c]) / 2)
-    paper += surface + smallest
-
-    volume = get_volume(dimensions)
-    perimeter = 2 * (x + y)
-    ribbon += volume + perimeter
+function read_lines_from_cwd(filename)
+    input_path = joinpath(dirname(Base.source_path()), filename)
+    f = open(input_path)
+    lines = readlines(f)
+    close(f)
+    lines
 end
-println("Paper: ", paper)
-println("Ribbon: ", ribbon)
+
+function main()
+    lines = read_lines_from_cwd("input.txt")
+    paper = 0
+    ribbon = 0
+    for line in lines
+        dimensions = split(line, "x")
+        dimensions = map(x->parse(Int32, x), dimensions)
+        sort!(dimensions)  # sort in-place
+        x, y, z = dimensions
+        a = 2 * x * y
+        b = 2 * x * z
+        c = 2 * y * z
+        surface = a + b + c
+        smallest = Int32(minimum([a, b, c]) / 2)
+        paper += surface + smallest
+
+        volume = get_volume(dimensions)
+        perimeter = 2 * (x + y)
+        ribbon += volume + perimeter
+    end
+    println("Paper: ", paper)
+    println("Ribbon: ", ribbon)
+end
+
+main()
