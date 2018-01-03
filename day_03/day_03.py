f49e81207213c2edfeb8dec7ce69460f28e43bd3
Fernando <fepegar@gmail.com>
2018-01-03 03:35:41 +0000

    Add second part of day 3

--- day_03/day_03.py
+++ day_03/day_03.py
@@ -8,7 +8,25 @@ WEST = '<'
 x, y = 0, 0
 gifts = 1
 visited = [(0, 0)]
-for char in data:
+
+for char in data[::2]:
+    if char == NORTH:
+        dx, dy = 0, 1
+    elif char == SOUTH:
+        dx, dy = 0, -1
+    elif char == EAST:
+        dx, dy = 1, 0
+    elif char == WEST:
+        dx, dy = -1, 0
+    x += dx
+    y += dy
+    location = x, y
+    if location not in visited:
+        gifts += 1
+        visited.append(location)
+
+x, y = 0, 0
+for char in data[1::2]:
     if char == NORTH:
         dx, dy = 0, 1
     elif char == SOUTH:
