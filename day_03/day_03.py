f44b0ff65d423c6acccdd0f975f05c3eef073898
Fernando <fepegar@gmail.com>
2018-01-03 03:35:26 +0000

    Add first part of day 3

---
+++ day_03/day_03.py
@@ -0,0 +1,27 @@
+from aocd import data
+
+NORTH = '^'
+SOUTH = 'v'
+EAST = '>'
+WEST = '<'
+
+x, y = 0, 0
+gifts = 1
+visited = [(0, 0)]
+for char in data:
+    if char == NORTH:
+        dx, dy = 0, 1
+    elif char == SOUTH:
+        dx, dy = 0, -1
+    elif char == EAST:
+        dx, dy = 1, 0
+    elif char == WEST:
+        dx, dy = -1, 0
+    x += dx
+    y += dy
+    location = x, y
+    if location not in visited:
+        gifts += 1
+        visited.append(location)
+
+print(gifts)
